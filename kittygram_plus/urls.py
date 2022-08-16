e4be15430f9abe10bfeb7efa115bbdcf4c0ce913
Никита Фреюк <freneek@gmail.com>
2022-08-16 18:54:26 +0300

    токены

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -9,4 +9,6 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
